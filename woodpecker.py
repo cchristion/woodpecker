45097966eeb34f799481c47ced16cdce4b9a95c4
cchristion <47944263+cchristion@users.noreply.github.com>
2023-10-31 15:37:15 +0530

    Sorted imports Better comments Added logging

--- woodpecker.py
+++ woodpecker.py
@@ -1,15 +1,16 @@
 """Script to recursively extract archives and compressed files."""
 
+import argparse
 import os
 import re
-import magic
-import argparse
 import subprocess
+import logging
+import magic
 
 from pathlib import Path
 
 def cli() -> dict[str, str]:
-    """Returns ."""
+    """Return parsed cli."""
     parser = argparse.ArgumentParser(prog="woodpecker",
         description="Recursively extract archives and compressed files.")
     parser.add_argument("directory", type=str, default="./"
@@ -17,25 +18,25 @@ def cli() -> dict[str, str]:
     return vars(parser.parse_args())
 
 def find_files(directory: str) -> list[str]:
-    """Finds all the files in a given directory."""
+    """Find all the files in a given directory."""
     files = []
     for dirpath, _, file_group in os.walk(directory):
         for file in file_group:
-            file = os.path.abspath(os.path.join(dirpath, file))
-            files.append(file)
+            abs_file = os.path.abspath(os.path.join(dirpath, file))
+            files.append(abs_file)
     return files
 
 def extract(files: list[str]) -> None:
-    """Extracts files from a given list of archive/compressed files."""
+    """Extract files from a given list of archive/compressed files."""
     for file in files:
-        print(f"Extracting {file}")
+        logging.info(f"Extracting {file}")
         try:
             subprocess.run(["7zz", "x", file, "-o"+file+"_dump"
                             , "-y", "-p1234", "-bse0", "-bso0"], check=True)
             Path(file).unlink()
-        except subprocess.CalledProcessError as e:
-            print(f"file: \'{file}\'\
-                Error type: {type(e)}\nError message: {e}\n")
+        except subprocess.CalledProcessError as error:
+            logging.info(f"file: \'{file}\'\
+                Error type: {type(error)}\nError message: {error}\n")
 
 if __name__ == "__main__":
 
